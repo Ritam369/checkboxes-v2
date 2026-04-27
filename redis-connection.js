c5c867a8a3f6363770e025a8e6f62fd2bce97775
Ritam Saha <ritamsaha75579@gmail.com>
2026-04-27 16:19:05 +0530

    Add support for REDIS_URL configuration in redis-connection.js

--- redis-connection.js
+++ redis-connection.js
@@ -1,6 +1,12 @@
 import Redis from "ioredis";
+import 'dotenv/config'
 
 const createChannel = () => {
+  // Use the REDIS_URL if it exists, otherwise fall back to localhost
+  if (process.env.REDIS_URL) {
+    return new Redis(process.env.REDIS_URL);
+  }
+  
   return new Redis({
     host: "localhost",
     port: 6379,
